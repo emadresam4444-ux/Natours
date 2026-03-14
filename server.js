d6fcdef30b7e91d8eb0d572c705ec219a94b0ad9
Emad-Eldeen <emadresam4444@gmail.com>
2026-03-14 06:53:51 +0200

    Add uncaught exception and unhandled rejection handlers to server

--- server.js
+++ server.js
@@ -1,19 +1,30 @@
+process.on('uncaughtException', err => {
+  console.log(err.name, err.message);
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  process.exit(1);
+});
+
 const dotenv = require('dotenv').config();
 const app = require('./app');
 const mongoose = require('mongoose');
 const DATABASE_URL = process.env.DATABASE;
 const PASSWORD = process.env.PASSWORD;
 const DB = DATABASE_URL.replace('<db_password>', PASSWORD);
-mongoose
-  .connect(DB)
-  .then(() => { 
-    console.log('DB connected');
-  })
-  .catch(err => {
-    console.log(err);
-  });
+
+
+mongoose.connect(DB).then(() => {
+  console.log('DB Connected ✅');
+});
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLE REJECTION! 💥 Shutting down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
